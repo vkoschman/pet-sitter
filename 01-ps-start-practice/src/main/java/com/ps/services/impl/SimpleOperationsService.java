e03acd156e2798528b60eea0d0c7f66bc3107f3f
vkoschman <207955@niuitmo.ru>
2018-09-06 17:46:39 +0400

    TODO 1 and 2 added (populating and creating Response instance)

--- 01-ps-start-practice/src/main/java/com/ps/services/impl/SimpleOperationsService.java
+++ 01-ps-start-practice/src/main/java/com/ps/services/impl/SimpleOperationsService.java
@@ -26,10 +26,18 @@ public class SimpleOperationsService implements OperationsService {
     public Response createResponse(Long sitterId, Long requestId) {
         // get sitter
         // TODO 1. retrieve sitter * request  (according to diagram 2.5)
+        User sitter = userRepo.findById(sitterId);
+        Request request = requestRepo.findById(requestId);
 
         //create a response
-        Response response = new Response();
         //TODO 2. populate & save the response object
+        Response response = new Response();
+        response.setUser(sitter);
+        response.setRequest(request);
+        response.setResponseStatus(ResponseStatus.PROPOSED);
+        response.setDetails("just sample text");
+        request.addResponse(response);
+        responseRepo.save(response);
         return response;
     }
 
--- 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
+++ 02-ps-container-01-practice/src/test/java/com/ps/beans/ctr/CIBeansTest.java
@@ -6,9 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by iuliana.cosmina on 3/26/16.
  */
